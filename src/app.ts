42ce445599f2733ee64837c8ccc2f9c7579b8d3d
Igor Kalinovskiy <jolly-good-fellow@yandex.ru>
2019-08-24 19:58:57 +1000

    Use pipe, compression, handle Link http header

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
+import { URL } from 'url'
 import qs from 'querystring'
-import express, { Application, RequestHandler } from 'express'
+import util from 'util'
+import stream from 'stream'
+
+import express, { Application, RequestHandler, Response as ExpressResponse } from 'express'
+import compression from 'compression'
 import { config } from 'dotenv'
-import fetch from 'node-fetch'
+import fetch, { Response as FetchResponse } from 'node-fetch'
+import LinkHeader from 'http-link-header'
+
+const pipeline = util.promisify(stream.pipeline)
 
 config()
 const baseUrl = 'https://api.github.com'
-const accessToken = process.env.GITHUB_TOKEN
+const access_token = process.env.GITHUB_TOKEN
 const port = process.env.PORT || 3000
 const app: Application = express()
 
-if (!accessToken) {
+if (!access_token) {
   throw Error('No GITHUB_TOKEN in environment')
 }
 
@@ -21,26 +29,52 @@ function withCatch(fn: RequestHandler): RequestHandler {
 
 function makeUrl(endpoint: string, params: { [key: string]: any }): string {
   const url = `${baseUrl}${endpoint}?${qs.stringify(params)}`
-  console.log(url)
+  //console.log(url)
   return url
 }
+
+async function pipeResponse(from: FetchResponse, to: ExpressResponse, host: string) {
+  const contentType = from.headers.get('Content-Type')
+  const link = from.headers.get('Link')
+  if (contentType) {
+    to.type(contentType)
+  }
+
+  if (link) {
+    const linkHeader = new LinkHeader(link)
+
+    for (const ref of linkHeader.refs) {
+      console.log(ref)
+      const url = new URL(ref.uri)
+      url.host = host
+      url.searchParams.delete('access_token')
+      ref.uri = url.toString()
+      console.log(ref)
+    }
+
+    to.links(linkHeader.refs.reduce((obj, ref) => ({ ...obj, [ref.rel]: ref.uri }), {}))
+  }
+
+  return pipeline(from.body, to)
+}
+
+app.use(compression())
 
 app.get(
   '/search/users',
   withCatch(async (req, res) => {
     const {
       path,
-      query: { q, per_page }
+      query: { q, per_page },
+      headers: { host }
     } = req
 
-    console.log(q, per_page)
-    console.log(path)
+    //console.log(req)
 
-    if (q && per_page) {
-      const data = await fetch(makeUrl(path, { q, per_page, accessToken })).then(resp =>
-        resp.json()
+    if (q && per_page && host) {
+      fetch(makeUrl(path, { q, per_page, access_token })).then(resp =>
+        pipeResponse(resp, res, host)
       )
-      res.json(data)
     } else {
       res.status(400).end()
     }
@@ -50,29 +84,32 @@ app.get(
 app.get(
   '/users/:username',
   withCatch(async (req, res) => {
-    const { path } = req
+    const {
+      path,
+      headers: { host }
+    } = req
 
-    const data = await fetch(makeUrl(path, { accessToken })).then(resp => resp.json())
-    res.json(data)
+    if (host) {
+      fetch(makeUrl(path, { access_token })).then(resp => pipeResponse(resp, res, host))
+    }
   })
 )
 
 //filter forks and repo fields here
 //handle links header
-//redirect data with streams?
 app.get(
   '/users/:username/repos',
   withCatch(async (req, res) => {
     const {
       path,
-      query: { page, per_page }
+      query: { page, per_page },
+      headers: { host }
     } = req
 
-    if (per_page) {
-      const data = await fetch(makeUrl(path, { page, per_page, accessToken })).then(resp =>
-        resp.json()
+    if (per_page && host) {
+      fetch(makeUrl(path, { page, per_page, access_token })).then(resp =>
+        pipeResponse(resp, res, host)
       )
-      res.json(data)
     } else {
       res.status(400).end()
     }
